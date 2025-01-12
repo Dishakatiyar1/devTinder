47727099db9d966b415e6cd656038f0386e3af3e
Disha Katiyar <77583219+Dishakatiyar1@users.noreply.github.com>
2025-01-13 00:21:32 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -4,20 +4,68 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Prashant",
-    lastName: "Badal",
-    emailId: "prashantbadal@gmail.com",
-    password: "Prashant@123",
-  };
   // creating a new instance of the user model
-  const user = new User(userObj);
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added successfully!!");
   } catch (err) {
     res.status(400).send("Error saving the user:", err.message);
+  }
+});
+
+// get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // const user = await user.findByIdAndDelete({ _id: userId });
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully.");
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "before",
+    });
+    console.log(user);
+    res.send("User updated successfully.");
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
+  }
+});
+
+// Feed API - GET /feed - get all the users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -3,21 +3,35 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    unique: true,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
   },
   gender: {
     type: String,
+  },
+  photoUrl: {
+    type: String,
+  },
+  about: {
+    type: String,
+    default: "This is the default about of the user!",
+  },
+  skills: {
+    type: [String],
   },
 });
 
