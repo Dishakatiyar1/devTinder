f5698bc863286364bb3cebdd4976abf75d3154b9
Disha Katiyar <77583219+Dishakatiyar1@users.noreply.github.com>
2025-03-29 17:54:26 +0530

    feed api & pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -52,4 +52,44 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // take query parameters
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser?._id }, { toUserId: loggedInUser?._id }],
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        {
+          _id: { $ne: loggedInUser._id },
+        },
+      ],
+    })
+      .skip(skip)
+      .limit(limit);
+    res.json({
+      message: "Data fetched successfully!",
+      data: users,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
 module.exports = userRouter;
